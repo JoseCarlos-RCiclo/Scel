6fb613bcf7d80ad7c1c1ca421b99030231bea203
JOSE CARLOS ALVES SANTOS JUNIOR <jose.junior@wds.local>
2019-10-01 10:17:19 -0300

    Versão 1.1

---
+++ scel/src/main/java/com/fatec/scel/model/Emprestimo.java
@@ -0,0 +1,55 @@
+package com.fatec.scel.model;
+
+import javax.persistence.*;
+
+@Entity(name = "Emprestimo")
+public class Emprestimo {
+	
+	@Id
+
+	private	Long id;
+	private String isbn;
+	private String usuario;
+	private String dataEmprestimo;
+	private String dataDevolucao;
+	private String dataDevolucaoPrevista;
+	
+		
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+	public void setDataEmprestimo(String dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+	public String getDataDevolucao() {
+		return dataDevolucao;
+	}
+	public void setDataDevolucao(String dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+	public String getDataDevolucaoPrevista() {
+		return dataDevolucaoPrevista;
+	}
+	public void setDataDevolucaoPrevista(String dataDevolucaoPrevista) {
+		this.dataDevolucaoPrevista = dataDevolucaoPrevista;
+	}
+	
+}
